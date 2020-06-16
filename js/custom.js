bc8b6732ad4b678aa1cf5dba1dd77dfabaaf14af
Aprila13 <aprilacraig1@gmail.com>
2020-06-16 15:55:27 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,3 @@
-// SCROLL BUTTON
-scroll = document.getElementById("topBtn");
-topBtn.addEventListener('click', topFunction);
-
-
-window.onscroll = function() {showScroll()};
-
-function showScroll() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scroll.style.display = "block";
-  } else {
-    scroll.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-
 // PRELOADER
 let preloader = document.getElementById('preloader');
 
